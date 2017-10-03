e4f82b3acea81548249c8e2ebef7397ad199362b
Nandha <nantha.sakthivel@tavant.com>
2017-10-03 15:04:26 +0530

    udpated chat server url to https

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -22,7 +22,7 @@ export class ChatComponent implements OnInit {
   public users: UserModel[] = [];
 
   constructor() {
-    this.socket = io(process.env.CHATSERVER || "http://35.198.223.173/");
+    this.socket = io(process.env.CHATSERVER || "https://35.198.223.173/");
     
     let user = new UserModel();
     user.name = this.userName;
